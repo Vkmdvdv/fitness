336782fefdc305398713fade062153e00b2f1b4f
vika <vi.medvedeva2008@gmail.com>
2025-12-19 19:57:39 +0300

    toogle

---
+++ src/components/ui/ThemeToggle.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { useThemeStore } from '@/stores/theme'
+import BaseButton from '@/components/ui/BaseButton.vue'
+
+const themeStore = useThemeStore()
+
+const toggleTheme = () => {
+  themeStore.toggleDarkMode()
+}
+</script>
+
+<template>
+  <BaseButton
+    @click="toggleTheme"
+    variant="text"
+    class="rounded-full p-2"
+    aria-label="Переключить тему"
+  >
+    <span v-if="themeStore.darkMode" class="i-mdi-moon-waning-crescent text-xl text-yellow-400"></span>
+    <span v-else class="i-mdi-white-balance-sunny text-xl text-orange-500"></span>
+  </BaseButton>
+</template>

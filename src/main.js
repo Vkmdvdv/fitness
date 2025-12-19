7737e9c2f84e43ee8969f37bdad112dffa01c825
vika <vi.medvedeva2008@gmail.com>
2025-12-19 20:31:57 +0300

    initializing a topic

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+import 'uno.css'
+import '@unocss/reset/tailwind.css'
+import { useThemeStore } from './stores/theme'
+
+const app = createApp(App)
+const pinia = createPinia()
+
+app.use(pinia)
+app.use(router)
+
+// Инициализация темы
+const themeStore = useThemeStore()
+themeStore.initTheme()
+
+app.mount('#app')
